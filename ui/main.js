c1c68cba5c3627ef041910cacbb0817218391160
Akash <kambleakash0@users.noreply.github.com>
2016-10-15 02:50:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //change text
 var elem = document.getElementById('main-text');
 elem.innerHTML="New View";
+//move image
+var img = document.getElementById('madi');
+ img.onClick() = function() {
+    img.style.marginLeft = '100px';
+};
