bae440c80fe41cdd44a115ef3a72ac27d30283a9
Akash <kambleakash0@users.noreply.github.com>
2016-10-15 01:31:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,30 +27,31 @@ function createTemplate(data){
     var heading=data.haeding;
     var content=data.content;
     var htmlTemplate = `
-    <html>
-    <head>
-        <title>{$title}</title>
-        <meta name="viewport" content="width=device-width,initial scale=1"/>
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-            <div>
-                <a href="/">Home</a>
+        <html>
+        <head>
+            <title>{$title}</title>
+            <meta name="viewport" content="width=device-width,initial scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                   {heading}
+                </h3>
+                <div>
+                    {$date}
+                </div>
+                <div>
+                    {$content}
+                </div>
             </div>
-            <hr/>
-            <h3>
-               {heading}
-            </h3>
-            <div>
-                {$date}
-            </div>
-            <div>
-                {$content}
-            </div>
-        </div>
-    </body>
-</html>`;
+        </body>
+        </html>`;
+    return htmlTemplate;
 }
 
 
@@ -60,7 +61,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res){
