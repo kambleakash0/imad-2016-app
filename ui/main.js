3afde8334d46c3f71728d364aac4285f0ddb1aba
Akash <kambleakash0@users.noreply.github.com>
2016-10-15 13:27:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,23 @@
 //
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function () {
-  //mkrqst
+  //createreq
+  var request = new XMLHttpRequest();
   
   //captres
+  request.onreadystatechange = function () {
+    if(request.readyState === XMLHttpRequest.DONE) {
+        //actn
+        if(request.status === 200) {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }  
+    //else
+  };
+  //mkrq
+  request.open('GET','http://kambleakash0.imad.hasura-app.io/counter',true);
+  request.send(null);
   
-  //renderinspan
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
 };
