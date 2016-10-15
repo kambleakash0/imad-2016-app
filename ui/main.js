b35f9f8b9ff3ccd89dfe9fcb0725a635d8767fa0
Akash <kambleakash0@users.noreply.github.com>
2016-10-15 12:55:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,13 @@
-console.log('Loaded!');
-//change text
-var elem = document.getElementById('main-text');
-elem.innerHTML="New View";
-
-//move image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-  var interval = setInterval(moveRight,50);  
+//
+var button = document.getElementById(counter);
+var counter = 0;
+button.onclick = function () {
+  //mkrqst
+  
+  //captres
+  
+  //renderinspan
+  counter = counter + 1;
+  var span = document.getElement.ById('count');
+  span.innerHTML = counter.toString();
 };
